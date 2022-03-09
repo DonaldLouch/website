51e9f8e35d6382f808fe5f9885860a610cb75a79
DevLexicon <contact@devlexicon.ca>
2022-03-09 11:43:46 -0800

    Font and NPM Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,8 +16,29 @@ import { theme } from "../theme/index"
 
 import { SessionProvider } from 'next-auth/react';
 
-import "@fontsource/lato"
-import "@fontsource/playfair-display"
+import '@fontsource/lato/100.css'
+import '@fontsource/lato/300.css'
+import '@fontsource/lato/400.css'
+import '@fontsource/lato/700.css'
+import '@fontsource/lato/900.css'
+import '@fontsource/lato/100-italic.css'
+import '@fontsource/lato/300-italic.css'
+import '@fontsource/lato/400-italic.css'
+import '@fontsource/lato/700-italic.css'
+import '@fontsource/lato/900-italic.css'
+
+import '@fontsource/playfair-display/400.css'
+import '@fontsource/playfair-display/500.css'
+import '@fontsource/playfair-display/600.css'
+import '@fontsource/playfair-display/700.css'
+import '@fontsource/playfair-display/800.css'
+import '@fontsource/playfair-display/900.css'
+import '@fontsource/playfair-display/400-italic.css'
+import '@fontsource/playfair-display/500-italic.css'
+import '@fontsource/playfair-display/600-italic.css'
+import '@fontsource/playfair-display/700-italic.css'
+import '@fontsource/playfair-display/800-italic.css'
+import '@fontsource/playfair-display/900-italic.css'
 
 import  { Layout }  from "../components/Layout"
 
