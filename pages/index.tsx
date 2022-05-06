2cfa8e7b7b7f2cb974e072f62460ded54d603c03
DevLexicon <contact@devlexicon.ca>
2022-05-06 16:05:25 -0700

    Movement To Donald Louch/Beta/Alpha Tracks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,42 @@
 import {Metadata} from "../components/Metadata"
+import HeroPage from "../components/HeroPage"
 
-
-import { Box, Flex, Heading, useColorModeValue, Image, Text, Link } from "@chakra-ui/react"
-
-import GradientBox from "../components/GradientBox"
-
-// import splitbee from '@splitbee/web'
-
-// import nookies from "nookies"
 
 export default function Home() {
-  // const action: string = 'action';
-  // const data: any = {};
-  // splitbee.track(action, data);
-
+    const pageLinks = [
+        {
+            'linkTitle': 'Blog', 
+            'linkUrl': 'blog'
+        },
+        {
+            'linkTitle': 'Portfolio', 
+            'linkUrl': 'portfolio'
+        },
+        {
+            'linkTitle': 'Resume', 
+            'linkUrl': 'portfolio/resume'
+        },
+        {
+            'linkTitle': 'Contact Me', 
+            'linkUrl': 'about#contact'
+        },
+        {
+            'linkTitle': 'Links', 
+            'linkUrl': 'about#links'
+        },
+        {
+            'linkTitle': 'Available For Freelance Jobs!', 
+            'linkUrl': 'jobs'
+        },
+    ]
   return (
     <>  
       <Metadata
-        title={`Welcome to ${process.env.WEBSITE_NAME}`}
-        keywords={`${process.env.KEYWORDS}`}
-        description={`${process.env.DESCRIPTION}`}
-      />
-
-      <Box 
-        as="section"
-        id="homeHero"
-
-        w="100vw" 
-        h="100vh"
-        maxW="100vw"
-        maxH="100vh"
-        pos="absolute" 
-        top="0" 
-        left="0"  
-        zIndex="overlay"
-        // overflowY="hidden"
-        // overflow="hidden"
-
-        boxShadow="bsOrange" 
-        bg={useColorModeValue("white", "black")}
-      >
-        <GradientBox />
-        <Flex id="heroWrap" pos="absolute" top="0" left="0" align="center" justify="center" w="100vw" h="100%" px="3rem" direction={{ base: 'column', lg: 'row' }}>
-          <Box>
-            <Image
-              // src="/titleLogo.svg"
-              src={useColorModeValue("/titleLogo.svg", "/titleLogoWhite.svg")}
-              alt="Donald Louch Title Logo"
-              width="35vw"
-              m="0.5rem auto"
-            />
-            <Heading as="h1" textAlign="center" fontSize="4rem" fontWeight="bold" color={useColorModeValue('primary', 'white')}>Welcome to Donald Louch!</Heading>
-            <Text textAlign="center" color={useColorModeValue('black', 'white')} fontSize="lg">Please note that at this time this website is under construction. Although, most features and content are available, some features may not or may not be fully developed.</Text>
-            <Text textAlign="center" color={useColorModeValue('black', 'white')} fontSize="lg" mt="1rem">You may <Link  href="homePage"  variant="primary">Continue To The Beta Version Here</Link>.</Text>
-          </Box>
-        </Flex>
-      </Box>
+          title={`${process.env.WEBSITE_NAME}`}
+          keywords={`${process.env.KEYWORDS}`}
+          description={`${process.env.DESCRIPTION}`}
+        />
+        <HeroPage name="Donald Louch" tagLine="and I'm a Canadian Digital Content Creator" links={pageLinks} cta={["About Me", "about"]} />
     </>
   )
 }
