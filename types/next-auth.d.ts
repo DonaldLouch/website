6b303042dd1e3f3d86e987c6c92a052f75219d8a
Donald Louch <hello@donaldlouch.ca>
2022-06-16 10:50:47 -0700

    ZoHo Intergration: Fix 2

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,19 +1,10 @@
-import { Session } from "next-auth"
-import { JWT } from "next-auth/jwt"
+import "next-auth";
 
-/** Example on how to extend the built-in session types */
 declare module "next-auth" {
-  interface Session {
-    /** This is an example. You can find me in types/next-auth.d.ts */
-    foo: string
+  interface User {
     userLevel: number
   }
-}
-
-/** Example on how to extend the built-in types for JWT */
-declare module "next-auth/jwt" {
-  interface JWT {
-    /** This is an example. You can find me in types/next-auth.d.ts */
-    bar: number
+  interface Session {
+    user: User;
   }
 }
