92999954b8deb97309409ee1804a8afff67b5d48
Donald Louch <hello@donaldlouch.ca>
2022-06-26 21:52:50 -0700

    Further Messages Implmentation: FIX 1

--- pages/api/pages/addNewPage.ts
+++ pages/api/pages/addNewPage.ts
@@ -1,36 +1,38 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-import prisma from '../../../config/prisma'
+import prisma from "../../../config/prisma";
 
-export default async function assetHandler(req: NextApiRequest, res: NextApiResponse) {
-    // if (req.method === 'POST') {
-    //     return res.status(405).json({ message: 'Method Not Allowed' })
-    // }
+export default async function assetHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // if (req.method === 'POST') {
+  //     return res.status(405).json({ message: 'Method Not Allowed' })
+  // }
 
-    try {
-        const postData = JSON.parse(req.body) as unknown|any|null
+  try {
+    const postData = JSON.parse(req.body) as unknown | any | null;
 
-        const post = await prisma.page.create({
-            data: {
-                id: postData.id,
-                title: postData.title,
-                slug: postData.slug,
-                body: postData.body,
-                excerpt: postData.excerpt,
-                tags: postData.tags,
-                pageStatus: postData.postStatus,
-                lastUpdatedOn: postData.postedOn,
-            },
-        })
+    const post = await prisma.page.create({
+      data: {
+        id: postData.id,
+        title: postData.title,
+        slug: postData.slug,
+        pageStatus: postData.postStatus,
+        lastUpdatedOn: postData.postedOn,
+      },
+    });
 
-        res.status(200).json(post)
-    } catch (error: any) {
-        // console.log(error.code)
-        if(error.code === "P2002") {
-            res.status(400).json({ message: 'Slug is already used. Try another slug.'})
-        } else {
-            // console.log(error)
-            res.status(500).json({ error })
-        }
+    res.status(200).json(post);
+  } catch (error: any) {
+    // console.log(error.code)
+    if (error.code === "P2002") {
+      res
+        .status(400)
+        .json({ message: "Slug is already used. Try another slug." });
+    } else {
+      // console.log(error)
+      res.status(500).json({ error });
     }
+  }
 }
