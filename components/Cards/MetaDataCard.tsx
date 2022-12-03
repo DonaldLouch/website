d7be08bb1445118069c996a7500bf1ae5fdf3b4e
Donald Louch <hello@donaldlouch.ca>
2022-12-03 09:05:12 -0800

    Version 28: Beta 2

--- components/Cards/MetaDataCard.tsx
+++ components/Cards/MetaDataCard.tsx
@@ -15,7 +15,7 @@ import {
     const postedOn = postedDay +" at " + postedTime
 
     return (
-        <Box bg="backgroundGradient" p="0.5rem 1rem" my="1.5rem" color="white">
+        <Box bg="backgroundGradient" p="0.5rem 1.5rem" my="1.5rem" color="white" borderRadius="0 2rem">
             <Text fontSize="1rem" color="white">By: <Link href="/about">{post.author}</Link> | Posted On: {postedOn} | Filed Under: {categorySplit.map((category: any) => (<Link key={category}href={`/C/${category}`} boxShadow="bsBoldWhite" p="0.5rem" borderRadius="0 0.5rem" mx="0.2rem" _hover={{boxShadow: "bsBoldOrange"}}>{category}</Link>))}</Text>
         </Box>
     )
