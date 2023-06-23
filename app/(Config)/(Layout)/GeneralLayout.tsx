559a5507baaae8b67901a201c02c65fd52d781b0
Donald Louch <hello@donaldlouch.ca>
2023-06-23 15:39:04 -0700

    Alpha Version 1.0

--- app/(Config)/(Layout)/GeneralLayout.tsx
+++ app/(Config)/(Layout)/GeneralLayout.tsx
@@ -12,6 +12,7 @@ export default function GeneralLayout({ children, isLoggedIn }: { children: Reac
             <Box as="main" bg="blurredBackground" minH="100vh" pt="5.8rem" pb="2rem" px={{base: "1rem", lg: "5rem"}} overflowX="clip" backdropBlur={"20px"} wordBreak="break-word" whiteSpace="break-spaces">
                 {children}
             </Box>
+            {/* @ts-ignore */}
             <Footer isLoggedIn={isLoggedIn} />
         </>
     )
