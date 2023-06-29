9ceb169da2676e1369b7befd6da06b5f085a6714
Donald Louch <hello@donaldlouch.ca>
2023-06-28 23:11:43 -0700

    Alpha Version 1.0.16

--- app/portal/pagesResumeExperience/AddEducation.tsx
+++ app/portal/pagesResumeExperience/AddEducation.tsx
@@ -1,64 +1,79 @@
 'use client'
 
 import { FormInput } from '@/app/(Components)/(Form)/FormInput'
+import { FormInputRow } from '@/app/(Components)/(Form)/FormInputRow'
 import { FormTextArea } from '@/app/(Components)/(Form)/FormTextArea'
-import { Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Stack, HStack, ModalFooter, Button, useDisclosure } from '@chakra-ui/react'
+import supabase from '@/lib/supabase'
+import { Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, Stack, HStack, ModalFooter, Button, useDisclosure, useToast } from '@chakra-ui/react'
 import { Formik } from 'formik'
 import { SubmitButton } from 'formik-chakra-ui'
 import * as Yup from 'yup'
 
 export default function AddEducation() {
-    const { isOpen, onClose } = useDisclosure()
+    const toast = useToast()
+    const toastID = "toastID"
+    
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const onSubmit =  async (values: any, actions: any) => {
-        // const { status: supabaseStatus , error: supabaseError } = await supabase.from("Resume").update({ 
-        //   lastUpdatedOn: updateResumeData.lastUpdatedOn 
-        // }).eq('id', updateResumeData.id)
-        // console.log(supabaseStatus)
-        // supabaseStatus && !toast.isActive(toastID) &&
-        //   toast({
-        //       id: toastID,
-        //       title: `${supabaseStatus === 204 ? "Page Updated 🎉" : `Error #${supabaseError?.code} has Occurred`}`,
-        //       description: `${supabaseStatus === 204 ? `You have successfully updated the Resume page!` : `An error has occurred: ${supabaseError?.message}. ${supabaseError?.hint && `${supabaseError?.hint}.`}`}`,
-        //       status: `${supabaseStatus === 204 ? "success" : "error"}`,
-        //       duration: 9000,
-        //       isClosable: true,
-        //   })
-        //   actions.setSubmitting(false)
+         const submitEducationData = {
+            id: "education"+Date.now().toString(36).toUpperCase() + Math.random().toString(36).substring(2, 5).toLowerCase(),
+            school: values.school,
+            degree: values.degree,
+            startDate: values.startDate,
+            endDate: values.endDate,
+            description: values.description
+         }
+        const { status: supabaseStatus , error: supabaseError  } = await supabase.from("ResumeEducation").insert({ 
+          id: submitEducationData.id,
+          school: submitEducationData.school,
+          degree: submitEducationData.degree,
+          startDate: submitEducationData.startDate,
+          endDate: submitEducationData.endDate,
+          description: submitEducationData.description,
+        })
+        supabaseStatus && !toast.isActive(toastID) &&
+          toast({
+              id: toastID,
+              title: `${supabaseStatus === 201 ? "Added New Education 🎉" : `Error #${supabaseError?.code} has Occurred`}`,
+              description: `${supabaseStatus === 201 ? `You have successfully updated the Resume page!` : `An error has occurred: ${supabaseError?.message}. ${supabaseError?.hint && `${supabaseError?.hint}.`}`}`,
+              status: `${supabaseStatus === 201 ? "success" : "error"}`,
+              duration: 9000,
+              isClosable: true,
+          })
+          actions.setSubmitting(false)
     }
-    const initialValues = {
-    }
-    const validationSchema = Yup.object({
-    })
+    const initialValues = {}
+    const validationSchema = Yup.object({})
     return (
-        <Modal isOpen={isOpen} onClose={onClose} id="addEduction" size="3xl">
-            <ModalContent>
-                <ModalHeader>Add Education</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                        {({ handleSubmit }: any) => (
-                            <Stack as="form" onSubmit={handleSubmit as any} px="1rem">
-                                <HStack spacing="2rem">
-                                    <FormInput inputID="school" inputLabel="School Name" inputType="text" />
-                                    <FormInput inputID="degree" inputLabel="Degree" inputType="text" />
-                                </HStack>
+        <>
+            <Button variant="primary" onClick={onOpen} background="primary"color="white" my="1rem !important">Add New Education</Button> 
+            <Modal isOpen={isOpen} onClose={onClose} id="addEduction" size="5xl">
+                <ModalContent background="blurredPurple">
+                    <ModalHeader>Add Education</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                            {({ handleSubmit }: any) => (
+                                <Stack as="form" onSubmit={handleSubmit as any} gap="1rem">
+                                    <HStack spacing="2rem">
+                                        <FormInputRow inputID="school" inputLabel="School Name" inputType="text" />
+                                        <FormInputRow inputID="degree" inputLabel="Degree" inputType="text" />
+                                    </HStack>
 
-                                <HStack spacing="2rem">
-                                    <FormInput inputID="startDate" inputLabel="Start Date" inputType="number" />
-                                    <FormInput inputID="endDate" inputLabel="End Date" inputType="number" />
-                                </HStack>
+                                    <HStack spacing="2rem">
+                                        <FormInputRow inputID="startDate" inputLabel="Start Date" inputType="number" />
+                                        <FormInputRow inputID="endDate" inputLabel="End Date" inputType="number" />
+                                    </HStack>
 
-                                <FormTextArea inputID="description" inputLabel="Description" textRows={4} />
+                                    <FormTextArea inputID="description" inputLabel="Description" textRows={4} />
 
-                                <SubmitButton variant="blackFormButton">Add Education</SubmitButton> 
-                            </Stack>
-                        )}
-                    </Formik>
-                </ModalBody>
-                <ModalFooter>
-                    <Button colorScheme='blue' mr={3} onClick={onClose}>Close</Button>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
+                                    <SubmitButton variant="blackFormButton" my="1rem !important">Add Education</SubmitButton> 
+                                </Stack>
+                            )}
+                        </Formik>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
--- app/portal/pagesResumeExperience/EditResumeExperiencePage.tsx
+++ app/portal/pagesResumeExperience/EditResumeExperiencePage.tsx
@@ -21,20 +21,20 @@ export default async function EditResumeExperiencePage({ resumeExperience, resum
     <>
       <BreadCrumb breads={breadCrumbs} />
       <Box as="main" id="homeWrapper" color="white">
-        {/* <Button  variant="primary" background="primary" color="white" my="1rem !important">Add Experience</Button>
-        <Button onClick={onEducationOpen} variant="primary"  background="primary"color="white" my="1rem !important">Add Education</Button> */}
+        <Button as="a" href={`/portfolio/resume`} variant="primary" background="primary" color="white" my="1rem !important">View Resume Page</Button>
         <Grid gridTemplateColumns="repeat(2, 1fr)" gap="2rem" mt="2rem">
           <Stack w="100%" rowGap="1rem">
+            <Button  variant="primary" background="primary" color="white" my="1rem !important" isDisabled>Add New Work Experience</Button>
             <SectionTitle headingTitle="Work Experience" />
             {resumeExperience?.map((experience: any) => ( <Button as="a" key={experience.id} variant="sectionButton" href={`resumeEditWork/${experience.id}`} justifyContent="left" pl="3rem" fontSize="1.2rem" fontWeight="500">{experience.company}</Button> ))}
           </Stack>
           <Stack w="100%" rowGap="1rem">
+            {/* <Button variant="primary"  background="primary"color="white" my="1rem !important">Add New Education</Button>  */}
+            <AddEducation />
             <SectionTitle headingTitle="Education" />
             {resumeEducation?.map((education: any) => ( <Button as="a" key={education.id} variant="sectionButton" href={`resumeEditEducation/${education.id}`} justifyContent="left" pl="3rem" fontSize="1.2rem" fontWeight="500">{education.school}</Button> ))}
           </Stack>
         </Grid>
-
-        <AddEducation />
       </Box>
     </>
   )
