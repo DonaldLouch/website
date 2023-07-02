76870c982f41b5e5756c459acfa1c4535ab75e3c
Donald Louch <hello@donaldlouch.ca>
2023-07-01 19:39:38 -0700

    Beta Version 1.3

--- app/(Components)/(Cards)/PinnedPostsCard.tsx
+++ app/(Components)/(Cards)/PinnedPostsCard.tsx
@@ -17,7 +17,7 @@ interface PinnedPostsProps {
 export default function PinnedPostsCard(post: PinnedPostsProps) {
     return (
         <Link href={`/post/${post.slug}`}>
-            <Card bg="none" boxShadow="bsBoldWhite" borderRadius="0 2rem" m="1rem 2rem" key={post.id} direction={{base: "column", md:"row"}} gap="1.5rem" alignItems="center" textAlign={{base: "center", md: "initial"}} pb={{base: "1rem", md: "0"}} pos="relative" p="0">
+            <Card bg="none" boxShadow="bsBoldWhite" borderRadius="0 2rem" m={{base: "1.5rem 1rem", lg: "1rem 2rem"}} key={post.id} direction={{base: "column", md:"row"}} gap="1.5rem" alignItems="center" textAlign={{base: "center", md: "initial"}} pb={{base: "2rem", md: "0"}} pos="relative" p="0" _hover={{background: "backgroundGradient"}}>
                 <Box color="white" pos="absolute" w="2%" zIndex="2" top="-19%" left="-1%" transform="rotate(-25deg)" display={{base: "none", md: 'initial'}} fontSize="3xl"><FontAwesomeIcon icon={['fal', 'thumbtack']} color="currentColor"/></Box>
                 <AspectRatio
                     ratio={16/9}
@@ -31,7 +31,7 @@ export default function PinnedPostsCard(post: PinnedPostsProps) {
                 </AspectRatio>
                 <Stack whiteSpace={{base: "initial", md: "nowrap"}} overflowX="scroll" w="60%">
                     <Heading color="white" m="0">{post.title}</Heading>
-                    <Text textShadow="3px 2px 4px rgb(193 93 79 / 20%)" fontSize="sm" color="gray.500" mt="-0.5rem">Posted On: {`${new Date(post?.postedOn).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} at ${new Date(post?.postedOn).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' })}`}</Text>
+                    <Text textShadow="3px 2px 4px rgb(193 93 79 / 20%)" fontSize="sm" color="gray.500" mt="-0.5rem" mb={{base: "2rem", md: "0"}}>Posted On: {`${new Date(post?.postedOn).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} at ${new Date(post?.postedOn).toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' })}`}</Text>
                 </Stack>
             </Card>
         </Link>
--- app/(Config)/(Layout)/GeneralLayout.tsx
+++ app/(Config)/(Layout)/GeneralLayout.tsx
@@ -9,7 +9,7 @@ export default function GeneralLayout({ children, isLoggedIn }: { children: Reac
     return (
         <>
             <Header />
-            <Box as="main" bg="blurredBackground" minH="100vh" pt="5.8rem" pb="2rem" px={{base: "1rem", lg: "5rem"}} overflowX="clip" backdropBlur={"20px"} wordBreak="break-word" whiteSpace="break-spaces">
+            <Box as="main" bg="blurredBackground" minH="100vh" pt={{base: "4.5rem", lg: "5.8rem"}} pb={{base: "1rem", lg: "2rem"}} px={{base: "1rem", lg: "5rem"}} overflowX="clip" backdropBlur={"20px"} wordBreak="break-word" whiteSpace="break-spaces">
                 {children}
             </Box>
             {/* @ts-ignore */}
