75f6ba757912a77ef41e076afadb2c8167d51a5e
Donald Louch <hello@donaldlouch.ca>
2023-07-03 18:26:56 -0700

    Beta Version 3.0.3

--- app/post/(Components)/PostContent.tsx
+++ app/post/(Components)/PostContent.tsx
@@ -5,21 +5,24 @@ import Loading from '@/app/(Config)/ContentLoading'
 import { MdxContent } from '@/app/mdx-content'
 import { serialize } from 'next-mdx-remote/serialize'
 import { type MDXRemoteSerializeResult } from 'next-mdx-remote'
-type Post = {
-  mdxSource: MDXRemoteSerializeResult
-}
+// type Post = {
+//   mdxSource: MDXRemoteSerializeResult
+// }
 
-async function getPostBody(postContent: string): Promise<Post> {
-  const mdxSource = await serialize(postContent, {mdxOptions: {
-    development: process.env.NODE_ENV === 'development',
-  }})
-  return {
-    mdxSource
-  }
-}
+// async function getPostBody(postContent: string): Promise<Post> {
+//   const mdxSource = await serialize(postContent, {mdxOptions: {
+//     development: process.env.NODE_ENV === 'development',
+//   }})
+//   return {
+//     mdxSource
+//   }
+// }
 
 export const PostContent = async ( post: any ) => { 
-  const { mdxSource } = await getPostBody(post.body!)
+  const mdxSource = await serialize(post.body!, {mdxOptions: {
+    development: process.env.NODE_ENV === 'development',
+  }}) as MDXRemoteSerializeResult
+  // const { mdxSource } = await getPostBody(post.body!)
   return (
     <Suspense fallback={<Loading />}>
       <MdxContent source={mdxSource} />
