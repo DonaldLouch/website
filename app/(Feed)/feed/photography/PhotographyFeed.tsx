0120fede2bdfe1afcf759a9746c6cc1c2557964d
Donald Louch <hello@donaldlouch.ca>
2024-03-16 14:17:33 -0700

    Initial Photo Feed Bug Fix

--- app/(Feed)/feed/photography/PhotographyFeed.tsx
+++ app/(Feed)/feed/photography/PhotographyFeed.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import supabase from "@/lib/supabase"
-import { debounce } from "lodash"
-import { useRef, useState, useEffect } from "react"
+// import { debounce, forEach } from "lodash"
+import { useState, useEffect, useRef } from "react"
 
 import {
   Box,
@@ -31,7 +31,9 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
-  Link
+  Link,
+  Spinner,
+  Heading
 } from "@chakra-ui/react";
 
 import Masonry from 'react-masonry-css'
@@ -47,11 +49,15 @@ import { InputControl } from "formik-chakra-ui";
 
 import { useDebounce } from 'use-debounce'
 import { BsChevronDown, BsFilter, BsSearch } from "react-icons/bs";
+import DisplayDate from "@/lib/DisplayDate";
+
+import { useInView } from 'react-intersection-observer'
 
 
 async function fetchPhotos(offset: number, limit: number, searchType?: string, searchValue?: string) {
   const from = offset * limit
   const to = from + limit - 1
+  console.log(from, to)
   const keyword = searchType === "tag" 
     && searchValue?.includes("HASHTAG") ? searchValue?.replace('HASHTAG', '#') 
     : searchValue?.includes("%20") ? searchValue?.replace('%20', ' ') 
@@ -74,46 +80,38 @@ async function fetchPhotos(offset: number, limit: number, searchType?: string, s
 }
 
 export default function PhotographyFeed({ photos, photographyAlbum, locationData, tagData, searchType, searchValue }: {photos: any, photographyAlbum: any, locationData: any, tagData: any, searchType?: string, searchValue?: string})  {
-  const toast = useToast()
-  const toastID = "toastID"
-  const PAGE_COUNT = 15 as number
-  const containerRef = useRef(null)
+  const PAGE_COUNT = 20 as number
   const initialRender = useRef(true)
   const [loadedPhotos, setLoadedPhotos] = useState(photos)
-  const [offset, setOffset] = useState(1)
+  const [page, setPage] = useState(0)
   const [isLoading, setIsLoading] = useState(false)
-  const [isInView, setIsInView] = useState(false)
   const router = useRouter()
 
-  // @ts-ignore
-  const handleScroll = (container) => {
-    if (containerRef.current && typeof window !== 'undefined') {
-      const container = containerRef.current
-      //@ts-ignore
-      const { bottom } = container.getBoundingClientRect()
-      const { innerHeight } = window
-      setIsInView(() => bottom <= innerHeight)
-    }
-  }
+  const [ref, inView] = useInView()
 
-  useEffect(() => {
-    debounce(() => handleScroll(containerRef), 200)
-    window.addEventListener('scroll', handleScroll)
-  }, [])
+  const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
-  useEffect(() => {
-    isInView && loadMorePhotos(offset)
-  }, [isInView])
-
-  const loadMorePhotos = async (offset: number) => {
+  async function loadMorePhotos() {
     setIsLoading(true)
-    setOffset((prev: number) => prev + 1)
-    const newPhotos = await fetchPhotos(offset, PAGE_COUNT, searchType, searchValue) as any
-    // setTimeout(()=>{
-    setLoadedPhotos((prevPhotos: any) => [...prevPhotos, ...newPhotos])
-    // }, 1000)
-    setIsLoading(false)
+    sleep(1000).then(async () => {
+      const nextPage = page + 1
+      const newPhotos = await fetchPhotos(nextPage, PAGE_COUNT, searchType, searchValue) as any
+      console.log(nextPage)
+
+      if (loadedPhotos?.length) {
+        setPage(nextPage)
+        setLoadedPhotos((prevPhotos: any) => [
+          ...(prevPhotos?.length ? prevPhotos : []),
+          ...newPhotos
+        ])
+      }
+      setIsLoading(false)
+    })
   }
+
+  useEffect(() => {
+    inView && loadMorePhotos()
+  }, [inView])
 
   const breakpointColumnsObj = {
 		default: 5,
@@ -122,6 +120,7 @@ export default function PhotographyFeed({ photos, photographyAlbum, locationData
 	}
   
   const { isOpen, onOpen, onClose } = useDisclosure()
+
   const [search, setSearch] = useState(searchValue)
   const [ focused, setFocused] = useState(false)
   const [ completed, setCompleted] = useState(false)
@@ -142,7 +141,7 @@ export default function PhotographyFeed({ photos, photographyAlbum, locationData
   }, [photos])
 
   return (<>      
-    <Box id="feed" pos="relative" w="100%" bg="blurredPurple" ref={containerRef} m={{base: "-4.5rem -1rem -1rem", lg: "-5.8rem -5rem -2rem"}} width="100vw" pt={{base: "0.5rem", lg: "1.5rem"}}>
+    <Box id="feed" pos="relative" w="100%" bg="blurredPurple" m={{base: "-4.5rem -1rem -1rem", lg: "-5.8rem -5rem -2rem"}} width="100vw" pt={{base: "0.5rem", lg: "1.5rem"}}>
       <Alert status='info' color="black" width="95vw" m="4.5rem auto 0">
         <Icon>
           <FontAwesomeIcon icon={["fas", "circle-info"]} color="currentColor"/>
@@ -164,22 +163,9 @@ export default function PhotographyFeed({ photos, photographyAlbum, locationData
         p="1rem"
         onClick={onOpen}
       >
-        <Tooltip label="Filters">
-         <BsFilter size="2rem" />
-          {/* <Icon w="2rem" h="auto"> */}
-            {/* <FontAwesomeIcon icon={["fas", "filter-list"]} width="100%" color="currentColor" /> */}
-          {/* </Icon> */}
-          {/* <IconButton
-              aria-label="Filters"
-              variant="unstyled"
-              _hover={{ color: "secondary" }}
-              color="white"
-              fontSize="3xl"
-              icon={<FontAwesomeIcon icon={["fas", "filter-list"]} />}
-              onClick={onOpen}
-          /> */}
-        </Tooltip>
+        <Tooltip label="Filters"><BsFilter size="2rem" /></Tooltip>
       </Box>
+
       <Drawer onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay />
         <DrawerContent background="blurredPurple" m="0.5rem" borderRadius="0 0 0 2rem">
@@ -202,12 +188,11 @@ export default function PhotographyFeed({ photos, photographyAlbum, locationData
                     type: "search"
                 }}
                 labelProps={{color: 'white'}}
-            /> */}
+            />  */}
             <Text>Search</Text>
             <InputGroup variant="unstyled" boxShadow='bsBoldSecondary' _focus={{boxShadow: "bsBoldPrimary"}} _invalid={{boxShadow: "bsBoldRed"}} color='white' borderRadius="0 1rem" my="1rem">
             <InputLeftElement pointerEvents='none' m="1.2rem 1rem">
               <BsSearch />
-              {/* <Icon><FontAwesomeIcon icon={["fas", "magnifying-glass"]} /></Icon> */}
             </InputLeftElement>
             <Input placeholder='Me' p="1rem 3rem" onFocus={() => {setFocused(true)}} onChange={(e: any) => {setSearch(e.target.value)}} onMouseOut={() => {setCompleted(true)}} />
             </InputGroup>
@@ -235,7 +220,7 @@ export default function PhotographyFeed({ photos, photographyAlbum, locationData
                   ))}
               </MenuList>
             </Menu>
-            {/* <Text>Tags:</Text> */}
+            <Text>Tags:</Text>
             <Menu>
               <MenuButton as={Button} rightIcon={<BsChevronDown />} width="100%" background="black" color="white" my="0.5rem" _hover={{background: "secondary"}} _active={{background: "secondary"}} borderRadius="0 0.5rem">
                 Tag Select
@@ -248,39 +233,40 @@ export default function PhotographyFeed({ photos, photographyAlbum, locationData
             </Menu>
             <Divider my="0.5rem" />
             <Text>Order:</Text>
-            {/* <Stack direction="row" gap="1rem" my="0.5rem"> */}
+            {/* <Stack direction="row" gap="1rem" my="0.5rem">  */}
               <Button hidden={searchValue != "old"} as={"a"} href={`/feed/photography`} width="100%" background="black" color="white" _hover={{background: "secondary"}} my="0.5rem" borderRadius="0 0.5rem">New-Old</Button>
               <Button hidden={searchType === "order" && searchValue === "old"} as={"a"} href={`/feed/photography?search=order&value=old`} width="100%" background="black" color="white" _hover={{background: "secondary"}} my="0.5rem" borderRadius="0 0.5rem">Old-New</Button>
-              {/* <Button as={"a"} href={`/feed/photography?search=order&value=random`} width="100%" background="black" color="white" _hover={{background: "secondary"}} isDisabled>Random</Button> */}
-            {/* </Stack> */}
+              {/* <Button as={"a"} href={`/feed/photography?search=order&value=random`} width="100%" background="black" color="white" _hover={{background: "secondary"}} isDisabled>Random</Button>  */}
+            {/* </Stack>  */}
             <Divider my="0.5rem" />
             <Text>View:</Text>
             <Button hidden={searchValue != "pinned"} as={"a"} href={`/feed/photography`} width="100%" background="black" color="white" _hover={{background: "secondary"}} my="0.5rem" borderRadius="0 0.5rem">All Photos</Button>
             <Button hidden={searchType === "view" && searchValue === "pinned"} as={"a"} href={`/feed/photography?search=view&value=pinned`} width="100%" background="black" color="white" _hover={{background: "secondary"}} my="0.5rem" borderRadius="0 0.5rem">Pinned Photos</Button>
           </DrawerBody>
         </DrawerContent>
-      </Drawer>
-      <Flex 
-        as={Masonry}
-        breakpointCols={breakpointColumnsObj}
-        // p="4.5rem 1rem 1rem"
-        p="1rem 1rem 1rem"
-        gap="0.5rem"
-      >
-        {isLoading && !toast.isActive(toastID) &&
-        toast({
-          id: toastID,
-          title: "Photos Loading",
-          description: "Additional photos are currently loading",
-          status: "info",
-          duration: 9000,
-          isClosable: true,
-        })}
-        {loadedPhotos?.map((image: any, index: number) => (<>
-          {/* <Text key={index} background="primary" p="2rem" my="1rem">(<DisplayDate source={image.fileID.takenOn} format="MM/DD/YY" />) {image.photoName}</Text> */}
-          <ViewPhotoFeed imageData={image} key={index} isLoading={isLoading} />
-        </>))}
-      </Flex>
+      </Drawer> 
+      <Box>
+        <Flex 
+          as={Masonry}
+          breakpointCols={breakpointColumnsObj}
+          // p="4.5rem 1rem 1rem"
+          p="1rem 1rem 1rem"
+          gap="0.5rem"
+        >
+          {loadedPhotos?.map((image: any, index: number) => (<>
+            {/* <Text key={index} background="primary" p="2rem" my="1rem">(<DisplayDate source={image.fileID.takenOn} format="MM/DD/YY" />) {image.id}</Text> */}
+            <ViewPhotoFeed imageData={image} key={index} isLoading={isLoading} />
+          </>))}
+        </Flex>
+      </Box>
+      <Stack  ref={ref} alignItems="center" p="2rem" color="white">
+        <Divider />
+          <Stack direction="row" gap="2rem" alignItems="center" pt="1rem" width="calc(100% * 4rem)">
+              <Spinner color="white" size="xl" />
+              <Heading fontSize={{base: "2rem", md: "3rem", lg:"5rem"}}>Loading More Photos</Heading>
+          </Stack>
+          <Text pt="1rem" textAlign="center">If the content is still not loaded after a minute please contact Donald Louch at <Link href="mailto:hello@donaldlouch.ca">hello@donaldlouch.ca</Link> for further assistance.</Text>
+      </Stack>
     </Box>
   </>)
 }
--- app/loading.tsx
+++ app/loading.tsx
@@ -8,7 +8,7 @@ export default function loading() {
         <Stack alignItems="center" minH="100vh" maxH="100vh" pt="2rem" w="100%" background="none" color="white">
             <Stack direction="row" gap={{base: "0.5rem", md: "2rem"}} alignItems="center">
                 <Spinner color="white" size="xl" />
-                <Heading fontSize="5rem">Loading Content</Heading>
+                <Heading  fontSize={{base: "2rem", md: "3rem", lg:"5rem"}}>Loading Content</Heading>
             </Stack>
             <Text pt="1rem">If the content is still not loaded after a minute please contact Donald Louch at <Link href="mailto:hello@donaldlouch.ca">hello@donaldlouch.ca</Link> for further assistance.</Text>
         </Stack>
