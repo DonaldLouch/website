adede5b870b48bf04aec8f1650f75106f40d4eb8
Donald Louch <hello@donaldlouch.ca>
2025-01-31 16:11:38 -0800

    version30.01.31.b1.21

--- app/(Admin)/admin/pagesResumeExperience/page.tsx
+++ app/(Admin)/admin/pagesResumeExperience/page.tsx
@@ -1,4 +1,4 @@
-import supabase from '@/lib/supabase'
+// import supabase from '@/lib/supabase'
 import EditResumeExperiencePage from './EditResumeExperiencePage'
 
 // import type { Metadata } from 'next'
@@ -13,8 +13,9 @@ import EditResumeExperiencePage from './EditResumeExperiencePage'
 // }
 
 export default async function PortalResumeExperiencePage() {
-  const { data: resumeExperience } = await supabase.from('ResumeWorkExperience').select().order('startDate', {ascending: false}) as any
-  const { data: resumeEducation } = await supabase.from('ResumeEducation').select().order('startDate', {ascending: false}) as any
-  const { data: resume } = await supabase.from('Resume').select("id").single() as any
-  return <EditResumeExperiencePage resumeExperience={resumeExperience} resumeEducation={resumeEducation} resumeID={resume.id} />
+  // const { data: resumeExperience } = await supabase.from('ResumeWorkExperience').select().order('startDate', {ascending: false}) as any
+  // const { data: resumeEducation } = await supabase.from('ResumeEducation').select().order('startDate', {ascending: false}) as any
+  // const { data: resume } = await supabase.from('Resume').select("id").single() as any
+  // resumeExperience={resumeExperience} resumeEducation={resumeEducation} resumeID={resume.id}
+  return <EditResumeExperiencePage />
 }
